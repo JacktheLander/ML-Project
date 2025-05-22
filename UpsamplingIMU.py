276da3b691e1d1cbd62ddd4c5b8068f8a770c987
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2025-05-21 22:12:22 -0700

    Update UpsamplingIMU.py

--- UpsamplingIMU.py
+++ UpsamplingIMU.py
@@ -5,7 +5,7 @@ def upsample(df):
 
   df['time'] = pd.to_timedelta(df['ACC X Time Series (s)'], unit='s')
   df = df.set_index('time')
-  IMU_upsampled = df[IMU_cols].resample('0.0007941176470588235s').asfreq()  # Scales these columns to be the same length as EMG data
+  IMU_upsampled = df[IMU_cols].resample('794117ns').asfreq()  # Scales these columns to be the same length as EMG data
   IMU_upsampled = IMU_upsampled.interpolate(method='linear')  # Interpolates the data using the linear method to match EMG data
   df_new = IMU_upsampled.join(df['EMG 1 (mV)'])
   return df_new
